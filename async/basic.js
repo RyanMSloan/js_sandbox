afcd6a52b8e9f636af0eb9b3637d50332d0c475b
DESKTOP-G0DVA23\Ryan <rsloan814@gmail.com>
2018-04-12 18:41:04 -0700

    Started async lectures

---
+++ async/basic.js
@@ -0,0 +1,30 @@
+// ASYNC
+
+document.getElementById('button').addEventListener('click', getData);
+
+function getData() {
+  // Create a XHR Object
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css', true);
+  xhr.send();
+  console.log(xhr.readyState);
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      const out = document.getElementById('output');
+      out.textContent = this.responseText;
+      //console.log(this.responseText);
+    }
+  }
+  
+  // Optionals
+  xhr.onprogress = function() {
+    // loader gif...
+    console.log(xhr.readyState);
+  }
+
+  xhr.onerror = function() {
+    console.log('Request Error...');
+  }
+}
